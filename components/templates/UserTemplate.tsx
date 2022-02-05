932cccfdeec32fe8eaf8e4f309e431d104750efb
Americo Chaquisse <achaquisse1@gmail.com>
2022-02-05 10:58:36 +0200

    Removed user tabs

--- components/templates/UserTemplate.tsx
+++ components/templates/UserTemplate.tsx
@@ -1,5 +1,4 @@
 import Header from '../molecules/Header';
-import UserTabs from '../molecules/UserTabs';
 
 interface UserTempleteProps {
   children: JSX.Element | JSX.Element[];
@@ -8,9 +7,7 @@ const UserTemplate = ({ children }: UserTempleteProps) => {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
       <Header />
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 pt-30 pb-12 md:pt-32">
-        <UserTabs />
-      </div>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 pt-30 pb-12 md:pt-32"></div>
       <main className="flex-grow">{children}</main>
     </div>
   );
